59bb53e3e26176a113df41a463dcc7dd4b055199
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-19 22:11:25 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 const app = express();
-const users = require("./Routes/user");
+const messages = require("./Routes/message");
+const mongoDB = require('./config/keys');
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api/users/', users);
+
+mongoose.connect(mongoDB.MongoURI, 
+    { useNewUrlParser: true })
+    .then(console.log("Connected to MongoDB"))
+    .catch(err => console.log(err));
+    
+app.use('/api/messages/', messages);
 
 app.listen(PORT, () => console.log(`Server is currently listening on PORT ${PORT}`))
