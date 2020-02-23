dea383cf7f565ed53dacd4d65452b23b983ee6f3
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-22 23:56:16 -0500

    updated XHR.js

---
+++ public/scriptXHR.js
@@ -0,0 +1,9 @@
+const sentHTTPRequest(method, url, data) => {
+    // Create a new XMLHTTPRequest()
+    let xhr = new XMLHttpRequest();
+    // Configure it pass method, url
+    xhr.open(method, url);
+    
+
+
+}
