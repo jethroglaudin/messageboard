1f9d51e4ddd8274bfa20e6cea80363cb54593a99
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-28 19:28:37 -0500

    updated scriptXHR.js

--- public/scriptXHR.js
+++ public/scriptXHR.js
@@ -1,29 +1,70 @@
-const sentHTTPRequest = async (method, url, data) => {
+const sendHTTPRequest = async (method, url, data) => {
     // Create a new XMLHTTPRequest()
-    let xhr = new XMLHttpRequest();
-    // Configure it pass method, url. Configure the request
-    xhr.open(method, url);
+    const promise = new Promise(( resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        // Configure it pass method, url. Configure the request
+        xhr.open(method, url);
+    
+        // We can use responseType property to set response format default is a string
+        // data will be parsed from json to javascript data. 
+        xhr.responseType = 'json';
+    
+        // Allows to both send and read customer headers from response
+        // Should you do a post request don't forget to set the header content type
+        // many server-side framworks 
+        if(data){
+            xhr.setRequestHeader('Content-type', 'application/json');
+        }
+        // this will be called after response is recieved 
+        xhr.onload = () => {
+            if(xhr.status >= 400) {
+                reject(xhr.response)
+                console.log(`Error ${xhr.status}: ${xhr.statusText}`)
+            }
+            else{
+                resolve(xhr.response);
+            }
+        }
+    
+        xhr.onerror = () => {
+            console.log('Something went wrong!');
+        }
+        
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;  
+}
 
-    // We can use responseType property to set response format default is a string
+const getAllMessages = async () => {
+    const url = "/api/messages/getallmessages";
+    let xhr = new XMLHttpRequest(); 
+    xhr.open("GET", url);
     xhr.responseType = 'json';
-
-    // Allows to both send and read customer headers from response
-    // Should you do a post request don't forget to set the header content type
-    // many server-side framworks 
-    if(data){
-        xhr.setRequestHeader('Content-type', 'application/json');
-    }
-    // this will be called after response is recieved 
+    xhr.send();
     xhr.onload = () => {
         if(xhr.status >= 400) {
-            console.log(`Error ${xhr.status}: ${xhr.statusText}`)
-        }
-    }
+            console.log(`Error: ${xhr.status}: ${xhr.statusText}`)
+        } else{
+            let response = xhr.response;
+            const message = response.map(data => {
+                return (
+                    "<li>"
+               + "Username: "
+               + data.userName
+               + "<br>"
+               + "<li>"
+               + "Email: "
+               + data.email
+               + "<br>"
+               + "<li>"
+               + "Message: "
+               + data.message
+           );               
+            })
+            document.getElementById("allMessages").innerHTML = `<ul> ${message.join("\n")} </ul>`; 
 
-    xhr.onerror = () => {
-        console.log('Something went wrong!');
-    }
-    
-    let responseData = await xhr.send(json(data));
-    return responseData;
+        }
+            
+    }        
 }
+
